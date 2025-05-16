9655d6f951e26280eddc06ea7ec030ffc1cb692a
GaylenLuo <132196533+GaylenLuo@users.noreply.github.com>
2025-05-16 16:06:30 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,13 @@ function Header() {
     <header className="sticky top-0 z-50 backdrop-blur-xl bg-indigo-50/80 text-xl px-4 py-2 flex border-b border-indigo-100 w-full">
       <Link className="flex items-center" to="/" onClick={scrollToTop}>
         <img alt="logo" className="w-8 h-8 mx-2" src={logo} />
-        <h1 className="font-semibold">CoverView</h1>
+        <h1 className="font-semibold">封面制作</h1>
       </Link>
 
       <div className="flex items-center md:gap-4 gap-2 ml-auto md:mr-4">
         <a
           className="bg-gray-700 hover:bg-gray-800 px-4 rounded-full p-1 text-white md:text-sm flex items-center text-xs font-Nunito"
-          href="https://github.com/Lruihao/CoverView"
+          href="https://github.com/GaylenLuo/CoverView"
           rel="noreferrer"
           target="_blank"
         >
